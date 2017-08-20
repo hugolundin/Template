f97938621838cb281cae45a414679f506f2e0556
Hugo Lundin <hugo@hugolundin.se>
2017-08-20 15:27:40 +0200

    Refactor Dependency Injection and Add Valid Init View Instantiation

---
+++ Template/Template/Model/Dependencies.swift
@@ -0,0 +1,16 @@
+//
+//  Clients.swift
+//  Template
+//
+//  Created by Hugo Lundin on 2017-08-12.
+//  Copyright © 2017 Hugo Lundin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Dependencies {
+    var todoist: Todoist?
+    var problem: String?
+    var settings = [Settings]()
+}
